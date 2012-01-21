7253133a94edf92725d7806f4d74abfe34e25da4
David Brown <adump@davidb.org>
2012-01-21 13:40:58 -0800

    Add 'main' invocation for 'du' command
    Also fixes the list command to use the new calling convention.

--- main.lisp
+++ main.lisp
@@ -2,13 +2,17 @@
 
 (defpackage #:ldump.main
   (:use #:cl #:iterate #:ldump #:ldump.file-pool #:ldump.nodes
-	#:command-line-arguments)
-  (:export #:main #:main-list))
+	#:command-line-arguments #:hashlib)
+  (:export #:main #:main-list #:main-tree-size))
 (in-package #:ldump.main)
 
 (defun main-list (pool-path)
   (with-pool (pool (cl-fad:pathname-as-directory pool-path))
-    (list-backups pool)))
+    (list-backups)))
+
+(defun main-tree-size (pool-path hash)
+  (with-pool (pool (cl-fad:pathname-as-directory pool-path))
+    (tree-size (unhexify hash))))
 
 (defun main ()
   (let ((args (get-command-line-arguments)))
@@ -16,4 +20,7 @@
     (cond ((and (= 2 (length args))
 		(string= (car args) "list"))
 	   (main-list (second args)))
+	  ((and (= 3 (length args))
+		(string= (car args) "du"))
+	   (main-tree-size (second args) (third args)))
 	  (t (format t "Usage: ldump command~%")))))
